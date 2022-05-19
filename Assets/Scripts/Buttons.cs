5907af093eade6a0ac441c51a69e6f5dcbe0715d
LuckyPiRaTe <69735696+LuckyPiRaTe@users.noreply.github.com>
2022-05-19 19:10:24 +0300

    UI

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public void CloseBatton(){
+        Application.Quit();
+    }
+}
